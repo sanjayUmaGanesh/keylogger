ebd9f45ad4b15c87af8cee9d5db4665f5e889632
sanjayUmaGanesh <69663787+sanjayUmaGanesh@users.noreply.github.com>
2022-12-13 05:28:03 -0800

    Add files via upload

---
+++ PRO_C226_StudentBolierPlate-main/send_email.py
@@ -0,0 +1,23 @@
+import smtplib, ssl
+
+def sendEmail(message):
+	smtp_server = "smtp.gmail.com"
+	port = 587 
+	sender_email = "umaganesh15@gmail.com"
+	password = "jeqlptyowfyndbqw"
+	receiver_email = "umaganesh15@gmail.com"
+
+	context = ssl.create_default_context()
+
+	try:
+
+		server = smtplib.SMTP(smtp_server,port)
+		server.starttls(context=context) 
+		server.login(sender_email, password)
+		server.sendmail(sender_email, receiver_email, message)
+	
+	except Exception as e: 
+		print(e) 
+	finally: 
+		server.quit()
+	
